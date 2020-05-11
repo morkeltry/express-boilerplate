d03cc5e7820f9e7b03dd8d44ef6ef1e130b9ad40
Mork <github@morkeltry.33mail.com>
2020-05-11 15:15:16 +0100

    nodemon restars cleanly using --SIGHUP caught in index.js to process.exit()

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ const app = require('./app');
 app.listen(app.get('port'), () => {
   console.log('App running on port', app.get('port'));
 });
+
+process.on('SIGHUP', () => { console.log("Bye bye!"); process.exit(); })
